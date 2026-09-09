1e4ba9f1c19736c55a350726e04ed86eff596e0e
maxime <maxlopez27140@gmail.com>
2026-09-09 18:37:02 +0200

    correction README

--- TP1/test.py
+++ TP1/test.py
@@ -1,9 +1,11 @@
 import pytest
 import fonctions as f
 
+
 def test_1():
     assert f.puissance(2, 3) == 8
     assert f.puissance(2, 2) == 4
+
 
 def test_2():
     assert f.puissance(-1, 2) == 1
@@ -11,10 +13,13 @@ def test_2():
     assert f.puissance(-1, -1) == -1
     assert f.puissance(-1, -2) == 1
     assert f.puissance(-2, -1) == -0.5
+
+
 def test_3():
     assert f.puissance(0, 1) == 0
     assert f.puissance(0, 2) == 0
     assert f.puissance(0, 5) == 0
+
 
 def test_exception():
     with pytest.raises(Exception):
