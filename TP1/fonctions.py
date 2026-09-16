d4054d5ba9c9bcd50eaf98ca4f7d2766b4e7c118
maxime <maxlopez27140@gmail.com>
2026-09-16 08:51:39 +0200

    puissance avec boucle for

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,11 +1,21 @@
 def puissance(a, b):
     if not type(a) is int:
-        raise TypeError("Only integers are allowed")
+        raise TypeError("Seuls les nombres entiers sont autorisés")
 
     if not type(b) is int:
-        raise TypeError("Only integers are allowed")
+        raise TypeError("Seuls les nombres entiers sont autorisés")
 
     if a == 0 and b < 0:
-        raise Exception("Operation indefinie")
+        raise Exception("Opération indéfinie")
 
-    return a ** b
+    resultat = 1
+
+    if b >= 0:
+        for i in range(b):
+            resultat = resultat * a
+    else:
+        for i in range(-b):
+            resultat = resultat * a
+        resultat = 1 / resultat
+
+    return resultat
