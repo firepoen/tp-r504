b0563688ec7da1a01c873c23366378d76c8876ff
maxime <maxlopez27140@gmail.com>
2026-09-21 10:56:42 +0200

    Mise à jour client HTTP TP9

--- tpjava/Clienthttp.java
+++ tpjava/Clienthttp.java
@@ -3,42 +3,46 @@ import java.net.*;
 
 public class Clienthttp
 {
-	public static void main(String[] args)
-	{
-		try
-		{
-			Socket socket = new Socket(args[0], 80);
+    public static void main(String[] args)
+    {
+        try
+        {
+            Socket socket = new Socket(args[0], 80);
 
-			OutputStreamWriter osw = new OutputStreamWriter(
-				socket.getOutputStream());
+            OutputStreamWriter osw =
+                new OutputStreamWriter(socket.getOutputStream());
 
-			InputStreamReader isw = new InputStreamReader(
-				socket.getInputStream());
+            InputStreamReader isw =
+                new InputStreamReader(socket.getInputStream());
 
-			BufferedWriter bufOut = new BufferedWriter(osw);
-			BufferedReader bufIn = new BufferedReader(isw);
+            BufferedWriter bufOut = new BufferedWriter(osw);
+            BufferedReader bufIn = new BufferedReader(isw);
 
-			String request = "GET / HTTP/1.0\r\n\r\n";
+            String request =
+                "GET / HTTP/1.1\r\n" +
+                "Host: " + args[0] + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
 
-			bufOut.write(request, 0, request.length());
-			bufOut.flush();
+            bufOut.write(request, 0, request.length());
+            bufOut.flush();
 
-			String line = bufIn.readLine();
+            String line = bufIn.readLine();
 
-			while(line != null)
-			{
-				System.out.println(line);
-				line = bufIn.readLine();
-			}
+            while(line != null)
+            {
+                System.out.println(line);
+                line = bufIn.readLine();
+            }
 
-			bufIn.close();
-			bufOut.close();
-			socket.close();
-		}
-		catch(Exception ex)
-		{
-			System.out.println("erreur !");
-			ex.printStackTrace();
-		}
-	}
+            bufIn.close();
+            bufOut.close();
+            socket.close();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("erreur !");
+            ex.printStackTrace();
+        }
+    }
 }
